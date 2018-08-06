03a1733b48bfd4db4c7f7c52d070486f8f09dccb
huzh <huzhihai@njdhy.com.cn>
2018-08-06 22:09:48 +0800

    初始化erureka-server

---
+++ src/main/java/cn/com/njdhy/springcloud/eurekaserver/Application.java
@@ -0,0 +1,20 @@
+package cn.com.njdhy.springcloud.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Eureka server 服务注册与发现 服务端
+ *
+ * @author jason.hu
+ * @date 2018-08-06
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
